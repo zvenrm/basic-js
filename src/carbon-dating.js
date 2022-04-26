78522d725962a063c58b2c986d7e43c04e15ccac
zvenrm <zvennnnnn@gmail.com>
2022-04-26 13:30:08 +0300

    feat: complete one task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(str) {
+    if(typeof(str) !== 'string' || Number.isNaN(+str) || +str > MODERN_ACTIVITY || +str <= 0){
+      return false
+    }
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +str) / (0.693 / HALF_LIFE_PERIOD))
 }
 
 module.exports = {
