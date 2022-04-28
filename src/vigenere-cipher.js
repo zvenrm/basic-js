c768e205067fcf78af8b79aca35b8c8d9d9c6c34
zvenrm <zvennnnnn@gmail.com>
2022-04-28 14:01:14 +0300

    feat: create one task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,110 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true){
+    this.reverse = reverse
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, keyWord) {
+    if(str === undefined || keyWord === undefined){
+      throw new NotImplementedError('Incorrect arguments!');
+    }
+
+    let alpha = []
+
+    for(let i = 65; i < 91; i++){
+        alpha.push(String.fromCharCode(i))
+    }
+
+    str = str.toUpperCase()
+    keyWord = keyWord.toUpperCase()
+
+    let strInd = []
+    let keyInd = []
+    for(let i = 0; i < str.length; i++){
+        if(!alpha.includes(str[i])){
+            strInd.push(str[i])
+        }
+        else{
+            strInd.push(alpha.indexOf(str[i]))
+        }
+    }
+
+    for(let i = 0; i < keyWord.length; i++){
+        if(alpha.includes(keyWord[i])){
+            keyInd.push(alpha.indexOf(keyWord[i]))
+        }
+    }
+
+    let keyIndex = 0
+    for(let i = 0; i < strInd.length; i++){
+        if(typeof(strInd[i]) === 'number'){
+            strInd[i] += keyInd[keyIndex]
+            if(strInd[i] > 25){
+                strInd[i] = alpha[strInd[i] - 26]
+            }else{
+                strInd[i] = alpha[strInd[i]]
+            }
+            
+            keyIndex++
+            if(keyIndex === keyInd.length){
+                keyIndex = 0
+            }
+        }
+        
+    }
+
+
+    if(this.reverse){
+      return strInd.join('')
+    }else{
+      return strInd.reverse().join('')
+    }
+  }
+  decrypt(str, keyWord) {
+    if(str === undefined || keyWord === undefined){
+      throw new NotImplementedError('Incorrect arguments!');
+    }
+    let alpha = []
+
+    for(let i = 65; i < 91; i++){
+        alpha.push(String.fromCharCode(i))
+    }
+    str = str.toUpperCase()
+    keyWord = keyWord.toUpperCase()
+
+    let strInd = []
+    let keyInd = []
+    for(let i = 0; i < str.length; i++){
+        if(!alpha.includes(str[i])){
+            strInd.push(str[i])
+        }
+        else{
+            strInd.push(alpha.indexOf(str[i]))
+        }
+    }
+    for(let i = 0; i < keyWord.length; i++){
+        if(alpha.includes(keyWord[i])){
+            keyInd.push(alpha.indexOf(keyWord[i]))
+        }
+    }
+
+    let keyIndex = 0
+    for(let i = 0; i < strInd.length; i++){
+        if(typeof(strInd[i]) === 'number'){
+            strInd[i] = alpha[(strInd[i] - keyInd[keyIndex] + 26) % 26]
+            
+            keyIndex++
+            if(keyIndex === keyInd.length){
+                keyIndex = 0
+            }
+        }
+        
+    }
+    if(this.reverse){
+      return strInd.join('')
+    }else{
+      return strInd.reverse().join('')
+    }
   }
 }
 
