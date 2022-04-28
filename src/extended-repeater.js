10188f092bba89518da921a19c97bc46fe12178f
zvenrm <zvennnnnn@gmail.com>
2022-04-28 09:53:44 +0300

    feat: create one task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  
+  let resStr = str
+    let additions = []
+    let res = []
+    if(options.hasOwnProperty('addition')){
+        if(options.hasOwnProperty('additionRepeatTimes')){
+            for(let i = 0; i < options.additionRepeatTimes; i++){
+                additions.push(String(options.addition))
+            }
+        }else{
+            additions.push(String(options.addition))
+        }
+    }
+    if(options.hasOwnProperty('additionSeparator')){
+        additions = additions.join(options.additionSeparator)
+    }else{
+        additions = additions.join('|')
+    }
+    resStr += additions
+    if(!options.hasOwnProperty('repeatTimes')){
+      res.push(resStr)
+    }else{
+      for(let i = 0; i < options.repeatTimes; i++){
+        res.push(resStr)
+      }
+    }
+    
+    if(options.hasOwnProperty('separator')){
+        return res.join(options.separator)
+    }
+    return res.join('+')
 }
 
 module.exports = {
